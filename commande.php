552828ef1d46ee89cb7004982573deb1662a216a
Matthias Michalet <matt.michalet@gmail.com>
2018-11-23 10:57:25 +0100

    Ajout de la page de Commande

---
+++ commande.php
@@ -0,0 +1,29 @@
+<!DOCTYPE html>
+<html>
+<head>
+	<title>Commander</title>
+</head>
+<body>
+	<fieldset>
+		<legend>Formulaire de commande</legend>
+		<form>
+			<label for="nom">Nom</label>
+				<input type="text" name="nom"></br>
+			<label for="prenom">Prénom</label>
+				<input type="text" name="prenom"></br>
+			<label for="mail">Adresse mail</label>
+				<input type="email" name="mail"></br>
+			<label for="rue">Rue</label>
+				<input type="text" name="rue"></br>
+			<label for="zipcode">Code postal</label>
+				<input type="text" name="zipcode"></br>
+			<label for="ville">Ville</label>
+				<input type="text" name="ville"></br>
+			<fieldset>
+				<legend>Choix des smoothies</legend>
+			</fieldset></br>
+			<input type="submit" name="valider" value="Valider">
+		</form>
+	</fieldset>
+</body>
+</html>
