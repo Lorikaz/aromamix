0c614deec5a5ce6740ae456b5590877e601228f8
socosta <sophie9796@hotmail.fr>
2018-11-23 11:07:27 +0100

    Add files via upload

--- index.php
+++ index.php
@@ -1,10 +1,22 @@
+<?php session_start(); ?>
 <!DOCTYPE html>
 <html>
 <head>
-	<title>Aromamix - Accueil</title>
+	<meta charset="utf-8" />
+    <title>Aromamix - Accueil</title>
+    <link rel="stylesheet" type="text/css" href="style.css" />
+    <link href="https://fonts.googleapis.com/css?family=Indie+Flower" rel="stylesheet">
 </head>
 <body>
 	<h1>Hello World !</h1>
 	<p>Ceci est une page test du site vitrine Aromamix</p>
+<body>
+    <div class="menu flex">
+        <div class="logo"><a href="Accueil.php">Accueil</a></div>
+        <div class="element-menu"><a href="plantes.php">Notre smoothies de base</a></div>
+        <div class="element-menu"><a href="#">Smoothies personnalisés</a></div>
+        <div class="element-menu"><a href="livreor.php">Livre d'or</a></div>
+        <div class="element-menu"><a href="panier.php"><img src="img/cart.png"></div>
+    </div>
 </body>
 </html>
