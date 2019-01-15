5ecc3fbf39638260c4fb9cae24c1cb8908ffdb63
Matthias Michalet <matt.michalet@gmail.com>
2019-01-15 17:48:16 +0100

    Backup before flex footer

--- parts/pied.php
+++ parts/pied.php
@@ -3,37 +3,37 @@
 
 	<nav class="footnav">
 		<div class="row">
-			<div class="col">
+			<div class="col-md-4" id="left">
 				<h2>Contactez-nous</h2>
 				<p>contact@aromamix.com
 				</br>+339 666 666</p>
 			</div>
-			<div class="col">
+			<div class="col-md-4">
 				<h2>Suivez-nous</h2>
 				<div class="row" id="social">
-					<div class="col-3">
+					<div class="social-logo col-3">
 						<a href="#">
 							<i class="fab fa-facebook"></i>
 						</a>
 					</div>
-					<div class="col-3">
+					<div class="social-logo col-3">
 						<a href="#">
 							<i class="fab fa-twitter"></i>
 						</a>
 					</div>
-					<div class="col-3">
+					<div class="social-logo col-3">
 						<a href="#">
 							<i class="fab fa-instagram"></i>
 						</a>
 					</div>
-					<div class="col-3">
+					<div class="social-logo col-3">
 						<a href="#">
 							<i class="fab fa-linkedin-in"></i>
 						</a>
 					</div>
 				</div>
 			</div>
-			<div class="col">
+			<div class="col-md-4" id="right">
 				<h2>Adresse</h2>
 				<p>13 rue de Salem
 				</br>13 666 France</p>
