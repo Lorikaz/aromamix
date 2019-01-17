1febc0f63947b6fc786321852f8201aefb51c6f1
Matthias Michalet <matt.michalet@gmail.com>
2019-01-17 17:03:27 +0100

    suppression des pages secondaires

--- commande.php
+++ commande.php
@@ -1,35 +1,33 @@
 <?php
 	session_start();
 
-  //URL of the host
-  $dbhost = "localhost";
+    //URL of the host
+    $dbhost = "localhost";
 
-  // Name of the database
-  $dbname = "aromamix";
+    // Name of the database
+    $dbname = "aromamix";
 
-  // User name
-  $dbuser = "root";
+    // User name
+    $dbuser = "root";
 
-  // Password (not used here)
-  $dbpass = "";
+    // Password (not used here)
+    $dbpass = "";
 
-  try {
-    $bdd = new PDO('mysql:host='.$dbhost.';dbname='.$dbname, $dbuser, $dbpass);
-  } catch(PDOException $e) {
-    echo $e->getMessage();
-  }
+    try {
+        $bdd = new PDO('mysql:host='.$dbhost.';dbname='.$dbname, $dbuser, $dbpass);
+    } catch(PDOException $e) {
+        echo $e->getMessage();
+    }
 
-  /* checks if user exists */
-  if (!empty($_SESSION['email']) && !empty($_SESSION['passsword'])) {
-    // this boolean variable will tell if the user is connected or just a visitor
-    $connect = true;
-  //rajouter condition pour afficher toute les données sessions de l'utilisateur ; id, firstname
-  }else{
-    $connect = false;
-  }
-  
-
-?>
+    /* checks if user exists */
+    if (!empty($_SESSION['email']) && !empty($_SESSION['passsword'])) {
+        // this boolean variable will tell if the user is connected or just a visitor
+        $connect = true;
+        //rajouter condition pour afficher toute les données sessions de l'utilisateur ; id, firstname
+    }else{
+        $connect = false;
+    }
+ ?>
 
 <!DOCTYPE html>
 <html>
@@ -40,12 +38,13 @@
 </head>
 
 <body>
-	<?php include("./parts/entete.php") ?>
+    <div class="page commande">
+      	<?php include("./parts/entete.php") ?>
 
-  
+        <section class="main-content"></section>
 
-	<?php include("./parts/pied.php") ?>
-	</div>
+      	<?php include("./parts/pied.php") ?>
+    </div>
 </body>
 
 </html>
--- potions.php
+++ potions.php
@@ -6,128 +6,161 @@
     <?php include("./parts/head.php") ?>
 </head>
 
-<body class="potions">
-    <?php include("./parts/entete.php") ?>
+<body>
+    <div class="potions">
+        <?php include("./parts/entete.php") ?>
 
 
-    <div class="title">
-        <h1>Nos 
-        <br/>potions</h1>
-        <img src="media/potions-header.png">
+        <section class="main-content">
+            <div class="title">
+                <h1>Nos 
+                <br/>potions</h1>
+                <img src="media/potions-header.png">
+            </div>
+
+            <div class="potion">
+                <div class="img">
+                    <img src="media/potions/PierreDeFlamel.png">
+                </div>
+                <div class="desc">
+                    <h1 class="nom">Pierre de Flamel</h1>
+                    <h2 class="sub">Notre célébrité.</h2>
+                    <p class="txt">Vous rajeunit automatiquement de dix ans ! Rêvez d'un corps jeune sans en perdre l'esprit mature.</p>
+                    <p class="warning">Attention, la potion n'augmente pas l'espérance de vie.</p>
+                    <div class="commande">
+                        <div class="cmd">
+                            <a href="commande.php">
+                                <h2>Commander</h2>
+                            </a>
+                        </div>
+                        <div class="prix">
+                            <h1>295</h1>
+                            <p>pièces d'or</p>
+                        </div>
+                    </div>
+                </div>
+            </div>
+
+            <div class="potion">
+                <div class="img">
+                    <img src="media/potions/Sanglote.png">
+                </div>
+                <div class="desc">
+                    <h1 class="nom">Sanglote</h1>
+                    <p class="sub"></p>
+                    <p class="txt">Retire toute forme de douleur. Physique ou moral, plus rien ne vous freinera.</p>
+                    <p class="warning">Attention, si prie en dehors des doses prescrites la vie pourra vous paraître terne.</p>
+                    <div class="commande">
+                        <div class="cmd">
+                            <a href="commande.php">
+                                <h2>Commander</h2>
+                            </a>
+                        </div>
+                        <div class="prix">
+                            <h1>145</h1>
+                            <p>pièces d'or</p>
+                        </div>
+                    </div>
+                </div>
+            </div>
+
+            <div class="potion">
+                <div class="img">
+                    <img src="media/potions/Creabilis.png">
+                </div>
+                <div class="desc">
+                    <h1 class="nom">Creabilis</h1>
+                    <h2 class="sub"></h2>
+                    <p class="txt">Donne à l'esprit vide la créativité de l'artiste.</p>
+                    <p class="warning">Elle peut être inspirante, toutefois, elle ne remplace pas l'imagination.</p>
+                    <div class="commande">
+                        <div class="cmd">
+                            <a href="commande.php">
+                                <h2>Commander</h2>
+                            </a>
+                        </div>
+                        <div class="prix">
+                            <h1>135</h1>
+                            <p>pièces d'or</p>
+                        </div>
+                    </div>
+                </div>
+            </div>
+
+            <div class="potion">
+                <div class="img">
+                    <img src="media/potions/Animalis.png">
+                </div>
+                <div class="desc">
+                    <h1 class="nom">Animalis</h1>
+                    <h2 class="sub"></h2>
+                    <p class="txt">Transforme votre anatomie en l'animal de l'esprit. A chacun son totem que vous pourrez incarner à volonté.</p>
+                    <p class="warning">Attention, nous n'engageons aucune responsabilité sur l'animal pouvant vous correspondre. -peut attirer les puces-</p>
+                    <div class="commande">
+                        <div class="cmd">
+                            <a href="commande.php">
+                                <h2>Commander</h2>
+                            </a>
+                        </div>
+                        <div class="prix">
+                            <h1>265</h1>
+                            <p>pièces d'or</p>
+                        </div>
+                    </div>
+                </div>
+            </div>
+
+            <div class="potion">
+                <div class="img">
+                    <img src="media/potions/Merliner.png">
+                </div>
+                <div class="desc">
+                    <h1 class="nom">Merliner</h1>
+                    <h2 class="sub"></h2>
+                    <p class="txt">Procure la sagesse et le savoir du vieux guides.</p>
+                    <p class="warning">Attention, il n'accepte pas les remises en question.</p>
+                    <div class="commande">
+                        <div class="cmd">
+                            <a href="commande.php">
+                                <h2>Commander</h2>
+                            </a>
+                        </div>
+                        <div class="prix">
+                            <h1>195</h1>
+                            <p>pièces d'or</p>
+                        </div>
+                    </div>
+                </div>
+            </div>
+
+            <div class="potion">
+                <div class="img">
+                    <img src="media/potions/FelixFelicis.png">
+                </div>
+                <div class="desc">
+                    <h1 class="nom">Felix Felicis</h1>
+                    <h2 class="sub"></h2>
+                    <p class="txt">Peut engager la chance auprès de celui qui s'en procure.</p>
+                    <p class="warning">Attention, le bonheur des uns fait le malheur des autres.</p>
+                    <div class="commande">
+                        <div class="cmd">
+                            <a href="commande.php">
+                                <h2>Commander</h2>
+                            </a>
+                        </div>
+                        <div class="prix">
+                            <h1>265</h1>
+                            <p>pièces d'or</p>
+                        </div>
+                    </div>
+                </div>
+            </div>
+
+        </section>
+
+
+        <?php include("./parts/pied.php") ?>
     </div>
-
-    <div class="row conteneur potion1">
-        <div class="col-md-6 element img" id="left">
-            <img src="media/potions/PierreDeFlamel.png">
-        </div>
-        <div class="col-md-6 element desc" id="right">
-     		<h1 class="nom_potion">Pierre de Flamel</h1>
-     		<p class="sous_titre">Notre célébrité.</p>
-     		<p class="txt">Vous rajeunit automatiquement de dix ans ! Rêvez d'un corps jeune sans en perdre l'esprit mature.</p>
-     		<p class="commentaire">Attention, la potion n'augmente pas l'espérance de vie.</p>
-            <a href="./commande.php" class="button">
-                <h2>Commander</h2>
-            </a>
-            <div class="prix">
-                <h1>295</h1>
-                <p>Pièces d'or</p>
-            </div>
-        </div>
- 	</div>
-
-    <div class="row conteneur potion2">
-        <div class="col-md-6 element img" id="right">
-            <img src="media/potions/Sanglote.png">
-        </div>
-            </a>
-        <div class="col-md-6 element desc" id="left">
-     		<h1 class="nom_potion">Sanglote</h1>
-     		<p class="txt">Retire toute forme de douleur. Physique ou moral, plus rien ne vous freinera.</p>
-     		<p class="commentaire">Attention, si prie en dehors des doses prescrites la vie pourra vous paraître terne.</p>
-            <a href="./commande.php" class="button">
-                <h2>Commander</h2>
-            </a>
-            <div class="prix">
-                <h1>145</h1>
-                <p>Pièces d'or</p>
-            </div>
-        </div>
- 	</div>
-
-    <div class="row conteneur potion3">
-        <div class="col-md-6 element img" id="left">
-            <img src="media/potions/Creabilis.png">
-        </div>
-        <div class="col-md-6 element desc" id="right">
-     		<h1 class="nom_potion">Créabilis</h1>
-     		<p class="txt">Donne à l'esprit vide la créativité de l'artiste.</p>
-     		<p class="commentaire">Elle peut être inspirante, toutefois, elle ne remplace pas l'imagination.</p>
-            <a href="./commande.php" class="button">
-                <h2>Commander</h2>
-            </a>
-            <div class="prix">
-                <h1>135</h1>
-                <p>Pièces d'or</p>
-            </div>
-        </div>
- 	</div> 	
-
-    <div class="row conteneur potion4">
-        <div class="col-md-6 element img" id="right">
-            <img src="media/potions/Animalis.png">
-        </div>
-        <div class="col-md-6 element desc" id="left">
-     		<h1 class="nom_potion">Animalis</h1>
-     		<p class="txt">Transforme votre anatomie en l'animal de l'esprit. A chacun son totem que vous pourrez incarner à volonté.</p>
-     		<p class="commentaire">Attention, nous n'engageons aucune responsabilité sur l'animal pouvant vous correspondre. -peut attirer les puces-</p>
-            <a href="./commande.php" class="button">
-                <h2>Commander</h2>
-            </a>
-            <div class="prix">
-                <h1>265</h1>
-                <p>Pièces d'or</p>
-            </div>
-        </div>
- 	</div> 	
-
-    <div class="row conteneur potion5">
-        <div class="col-md-6 element img" id="left">
-            <img src="media/potions/Merliner.png">
-        </div>
-        <div class="col-md-6 element desc" id="right">
-     		<h1 class="nom_potion">Merliner</h1>
-     		<p class="txt">Procure la sagesse et le savoir du vieux guides.</p>
-     		<p class="commentaire">Attention, il n'accepte pas les remises en question.</p>
-            <a href="./commande.php" class="button">
-                <h2>Commander</h2>
-            </a>
-            <div class="prix">
-                <h1>195</h1>
-                <p>Pièces d'or</p>
-            </div>
-        </div>
- 	</div> 	
-
-    <div class="row conteneur potion6">
-        <div class="col-md-6 element img" id="right">
-            <img src="media/potions/FelixFelicis.png">
-        </div>
-        <div class="col-md-6 element desc" id="left">
-     		<h1 class="nom_potion">Felix Felicis</h1>
-     		<p class="txt">Peut engager la chance auprès de celui qui s'en procure.</p>
-     		<p class="commentaire">Attention, le bonheur des uns fait le malheur des autres.</p>
-            <a href="./commande.php" class="button">
-                <h2>Commander</h2>
-            </a>
-            <div class="prix">
-                <h1>265</h1>
-                <p>Pièces d'or</p>
-            </div>
-        </div>
- 	</div> 	
-
-
-    <?php include("./parts/pied.php") ?>
 </body>
 
 </html>
