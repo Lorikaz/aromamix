d6a369a629ad0f6b83e2f10a0f1c2abebbd73ccf
Matthias Michalet <matt.michalet@gmail.com>
2019-01-20 20:40:31 +0100

    Corrections de dernière minute

--- js/sidenav.js
+++ js/sidenav.js
@@ -1,9 +1,9 @@
 /* Open when someone clicks on the span element */
 function openNav() {
-    document.getElementById("sidenav").style.width = "100%";
+    document.getElementById("sidenav").style.width = "100vw";
 }
 
 /* Close when someone clicks on the "x" symbol inside the overlay */
 function closeNav() {
-    document.getElementById("sidenav").style.width = "0%";
+    document.getElementById("sidenav").style.width = "0vw";
 }
